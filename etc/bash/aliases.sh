7dc52ed971801ea65c8e637ab0c7f53bde55136e
Tom <tom@degrunt.nl>
2010-10-15 00:04:14 +0200

    Moving stuff back and forth

---
+++ etc/bash/aliases.sh
@@ -0,0 +1,19 @@
+# ===========
+# = Aliases =
+# ===========
+
+# some more ls aliases
+alias ls='ls -G'
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+# Reload bash (rebash)
+alias rebash='source $HOME/.bashrc'
+
+# Start screensaver
+alias ss="osascript -e 'tell application id \"com.apple.ScreenSaver.Engine\" to launch'"
+
+# Git
+alias gs="git status"
+
---
+++ etc/bash/path.sh
@@ -0,0 +1,10 @@
+PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin
+
+PATH=/usr/local/Cellar/ruby/1.9.2-p0/bin:$PATH
+PATH=/usr/local/sbin:$PATH
+PATH=/usr/local/share/npm/bin:$PATH
+export PATH
+
+MAN_PATH=$MAN_PATH:/usr/local/Cellar/node/0.2.3/share/man
+export MAN_PATH
+
