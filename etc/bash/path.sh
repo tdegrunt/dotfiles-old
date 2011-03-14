c03aa8e0a94c6d1997f2d496f1e6055c0ec5a299
Tom <tom@degrunt.nl>
2011-03-14 22:05:37 +0100

    Added ruby path

--- etc/bash/path.sh
+++ etc/bash/path.sh
@@ -3,6 +3,7 @@ PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin
 PATH=/usr/local/Cellar/ruby/1.9.2-p0/bin:$PATH
 PATH=/usr/local/sbin:$PATH
 PATH=/usr/local/share/npm/bin:$PATH
+PATH=/usr/local/Cellar/ruby/1.9.2-p180/bin:$PATH
 export PATH
 
 MAN_PATH=$MAN_PATH:/usr/local/Cellar/node/0.2.3/share/man
